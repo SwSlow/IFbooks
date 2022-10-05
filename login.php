5ca662675d8028a62400a2485502092fb9a9c1b6
joaoartur297 <joao.2020302933@aluno.iffar.edu.br>
2022-10-05 09:48:07 -0300

    teste

---
+++ login.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<link rel="stylesheet" href="css/style.css"/>
+
+</head>
+<meta charset="utf-8"> 
+
+<title>Login</title>    
+ 
+
+<body>
+
+
+<h4>Bem vindo!</h4>
+
+<!-- painel login -->
+
+<div>
+
+<br>
+
+<img class="logo" src="imagens/Vector.png">
+<br><br><br><br>
+
+<h1>Endereço de email</h1>
+
+<input type="text" placeholder="you@youremail.com">
+
+<h1>Senha</h1>
+
+<input type="password" placeholder="Senha">
+
+<br><br>
+
+<button><h2>Entrar</h2></button> <h3>Esqueçeu sua senha?</h3>
+
+</div>
+
+<img class="linha" src="imagens/linha.png">
+<img class="linha2" src="imagens/linha.png">
+
+<!-- botões -->
+
+<button class="cadastrar"><h2>Cadastrar</h2></button>
+<button class="entrar"><h2>Entrar</h2></button> 
+
+
+<?php
+
+
+?>
+
+
+</body>
+ 
+<footer>
+<img src="imagens/ondaas.PNG"  width="100%" height="50%">
+</footer>
+ 
+</html>
