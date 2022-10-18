0ca850b088653d5278aeee7bcf89ff09d8ce2911
Luis Gustavo <luisgustavo3010@hotmail.com>
2022-10-18 09:42:04 -0300

    mudanças gerais

--- cadastra.php
+++ cadastra.php
@@ -9,7 +9,7 @@ $email = $_POST['email'];
 $curso = $_POST['curso'];
 $biblioteca = $_POST['biblioteca'];
 $situacao = $_POST['situacao'];
-$acesso = $_POST['acesso'];
+$acesso = $_POST['tipo_user'];
 $user = $_POST['tipo_user'];
 $senha = $_POST['senha'];
 
@@ -28,8 +28,12 @@ $senha = $_POST['senha'];
 <?php echo $senha; ?><br><br>
 
 <?php
-$result = "INSERT INTO usuarios (nome, num_matricula, cpf, email, curso, bibioteca, situação, niveis_acesso_id, user, senha) 
-                       VALUES ('$nome', '$matricula', '$cpf', '$email', '$curso, '$biblioteca', '$situacao', '$acesso', '$user', '$senha')";
+
+$result = "INSERT INTO  `usuários` (`nome`, `num_matricula`, `cpf`, `email`, `curso`, `biblioteca`, `situacao`, `niveis_acesso_id`, `user`, `senha`)
+                         VALUES('$nome', '$matricula', '$cpf', '$email', '$curso', '$biblioteca', '$situacao', '$user', '$senha')";
+
+INSERT INTO `usuarios` (`nome`, `num_matricula`, `cpf`, `email`, `curso`, `biblioteca`, `situacao`, `niveis_acesso_id`, `user`, `senha`) 
+            VALUES (NULL, 'Luis', '2020303019', '55555555', 'ascascasc', 'info', 'FW', 'regular', '2', '2', '123');
 
 $insert = mysqli_query($conn, $result) or die(" O sistema não foi capaz de realizar a operação!");
 
--- cadastro.php
+++ cadastro.php
@@ -13,7 +13,6 @@
   <img class="fundos" src="imagens/fundo.png">
   <img class="logoIff" src="imagens/IFF.png">
 
-
   <!-- painel login -->
 
   <div class="div_cadastro">
@@ -35,13 +34,10 @@
         <h1>Senha</h1>
         <input type="number" name="senha" id="senha" placeholder="Senha">
 
-        <h1>Acesso</h1>
-        <input type="text" name="acesso" id="acesso" placeholder="Acesso">
-
         <h1 class="positionCurso">Curso</h1>
-        
+
         <div class="cursos">
-          <select name="curso" id="curso" >
+          <select name="curso" id="curso">
             <option selected disabled class="opNon">Indique seu curso</option>
             <option value="valor1">Valor 1</option>
             <option value="valor2">Valor 2</option>
@@ -55,12 +51,12 @@
           <select name="biblioteca" id="biblioteca" required>
             <option selected disabled class="opNon">Indique seu campus</option>
             <option value="valor1">Valor 1</option>
-            <option value="valor2" >Valor 2</option>
+            <option value="valor2">Valor 2</option>
             <option value="valor3">Valor 3</option>
           </select>
         </div>
 
-        <h1 class="positionSituacao">Categoria de usuário</h1>
+        <h1 class="positionSituacao">Atual</h1>
 
         <div class="formSituacao" id="situacao">
           <input class="raioUser" type="radio" id="afastado" name="situacao" value="afastado">
@@ -70,80 +66,19 @@
         </div>
 
         <h1 class="positionUserC">Categoria de usuário</h1>
-        
+
         <div class="formUser" id="user">
-          <input class="raioUser" type="radio" id="servidor" name="tipo_user" value="servidor">
+          <input class="raioUser" type="radio" id="servidor" name="tipo_user" value="2">
           <label class="labelUser" for="servidor">Servidor</label>
-          <input class="raioUser1" type="radio" id="estudante" name="tipo_user" value="estudante">
+          <input class="raioUser1" type="radio" id="estudante" name="tipo_user" value="13">
           <label class="labelUser1" for="estudante">Estudante</label>
         </div>
         <br><br><br>
-        
+
         <button type="submit">
           <h2>Cadastrar</h2>
         </button>
       </form>
-
-      <!-- <img class="logo" src="imagens/Vector.png"> -->
-
-      <!-- <h1>Nome completo</h1>
-
-      <input type="text" placeholder="nome">
-
-      <h1>Numero de Matrícula</h1>
-
-      <input type="text" placeholder="Num_matrícula">
-
-      <h1>CPF</h1>
-
-      <input type="number" placeholder="CPF">
-
-      <h1>Email Institucional</h1>
-
-      <input type="email" placeholder="Email"> -->
-
-      <!-- <h1 class="positionCurso">Curso</h1>
-
-      <div class="cursos">
-        <select name="Curso">
-          <option selected disabled class="opNon">Indique seu curso</option>
-          <option value="valor1">Valor 1</option>
-          <option value="valor2" selected>Valor 2</option>
-          <option value="valor3">Valor 3</option>
-        </select>
-      </div> -->
-
-      <!-- <h1 class="positionCampus">Campus</h1>
-
-      <div class="BibVinc">
-        <select name="Biblioteca">
-          <option selected disabled class="opNon">Indique seu campus</option>
-          <option value="valor1">Valor 1</option>
-          <option value="valor2" selected>Valor 2</option>
-          <option value="valor3">Valor 3</option>
-        </select>
-      </div> -->
-
-      <!-- <h1 class="positionSituacao">Categoria de usuário</h1>
-
-
-      <div class="formSituacao">
-
-        <input class="raioUser" type="radio" id="afastado" name="tipo_user" value="afastado">
-        <label class="labelUser" for="afastado">Afastado</label>
-        <input class="raioUser1" type="radio" id="regular" name="tipo_user" value="regular">
-        <label class="labelUser1" for="regular">Regular</label>
-      </div>
-
-      <h1 class="positionUserC">Categoria de usuário</h1>
-
-      <div class="formUser">
-
-        <input class="raioUser" type="radio" id="servidor" name="tipo_user" value="servidor">
-        <label class="labelUser" for="servidor">Servidor</label>
-        <input class="raioUser1" type="radio" id="estudante" name="tipo_user" value="estudante">
-        <label class="labelUser1" for="estudante">Estudante</label>
-      </div> -->
 
     </div>
 
@@ -159,12 +94,6 @@
   <button class="cadastrar2">
     <h2>Cadastrar</h2>
   </button>
-
-  <?php
-
-
-  ?>
-
 
 </body>
 
