8144a9daa5cb5b2d86fc80aed6789f391a3b48d1
Luis Gustavo <luisgustavo3010@hotmail.com>
2022-10-19 19:40:42 -0300

    alterações gerais

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+define('BD_USER', 'root'); // USE O TEU USU�RIO DE BANCO DE DADOS
+define('BD_PASS', ''); // USE A TUA SENHA DO BANCO DE DADOS
+define('BD_NAME', 'users'); // USE O NOME DO TEU BANCO DE DADOS
+
+mysqli_connect('localhost', BD_USER, BD_PASS);
+mysqli_select_db(BD_NAME);
+
+
+// define('BD_USER', 'anatolibras'); // USE O TEU USU�RIO DE BANCO DE DADOS
+// define('BD_PASS', 'Y6ds6r3'); // USE A TUA SENHA DO BANCO DE DADOS
+// define('BD_NAME', 'anatolibras'); // USE O NOME DO TEU BANCO DE DADOS
+
+// mysql_connect('localhost', BD_USER, BD_PASS);
+// mysql_select_db(BD_NAME);
+
+
+
+?>
