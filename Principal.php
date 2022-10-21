613e12e019131750c4422175a0e5d65ef9422178
joaoartur297 <joao.2020302933@aluno.iffar.edu.br>
2022-10-21 11:20:13 -0300

    arquivos principal

---
+++ Principal.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link rel="stylesheet" href="css/stylePrincipal.css"/>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+
+</head>
+<meta charset="utf-8">
+
+<title>Página Principal</title>
+
+
+
+<body>
+<header class="cabecalho">
+    <img class="logoBib" src="imagens/Logo.png">
+    <img class="UserBib" src="imagens/User.png">
+            <div class="search-box">
+            <input class="search-txt" type="text" name="" placeholder="Digite sua pesquisa">
+            <a class="search-btn" href="#">
+                <i class="fas fa-search"></i>
+             </a>
+            </div>
+</header>
+
+</body>
+
+<footer>
+  <img src="imagens/ondaas.PNG" width="100%" height="50%">
+</footer>
+
+</html>
